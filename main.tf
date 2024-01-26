da8a794837c5ea0783b9c325f53c797ef3e60c7f
Amir Tayabali <58569032+Amir-Tayabali@users.noreply.github.com>
2024-01-26 16:33:09 +0000

    Proper DNS configuration for Private Endpoint

--- main.tf
+++ main.tf
@@ -130,6 +130,13 @@ resource "azurerm_private_endpoint" "state" {
     subresource_names = ["blob"]
   }
 
+
+  private_dns_zone_group {
+    name                 = "example-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.example.id]
+  }
+
+
   tags = var.tags
 
   lifecycle {
@@ -138,4 +145,20 @@ resource "azurerm_private_endpoint" "state" {
       private_dns_zone_group
     ]
   }
+}
+
+
+resource "azurerm_private_dns_zone" "example" {
+  count               = var.private_endpoint_subnet_id == null ? 0 : 1
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.state.name
+}
+
+
+resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+  count               = var.private_endpoint_subnet_id == null ? 0 : 1
+  name                  = "example-link"
+  resource_group_name   = azurerm_resource_group.state.name
+  private_dns_zone_name = azurerm_private_dns_zone.example.name
+  virtual_network_id    = var.virtual_network_id
 }
