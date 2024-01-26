0c57e132f3f660895363d16117806fae8141afac
Amir Tayabali <58569032+Amir-Tayabali@users.noreply.github.com>
2024-01-26 17:05:09 +0000

    Amendment

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ resource "azurerm_private_endpoint" "state" {
 
   private_dns_zone_group {
     name                 = "example-dns-zone-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.example.id]
+    private_dns_zone_ids = [azurerm_private_dns_zone.example[count.index].id]
   }
 
 
@@ -159,6 +159,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "example" {
   count                 = var.private_endpoint_subnet_id == null ? 0 : 1
   name                  = "example-link"
   resource_group_name   = azurerm_resource_group.state.name
-  private_dns_zone_name = azurerm_private_dns_zone.example[count.name]
+  private_dns_zone_name = azurerm_private_dns_zone.example[count.index].name
   virtual_network_id    = var.virtual_network_id
 }
